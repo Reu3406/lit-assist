024908b96eb1c5e63189c3140144556063aafb65
Reu3406 <157335953+Reu3406@users.noreply.github.com>
2024-02-29 11:04:02 +0800

    Update app.py
    overhaul

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-import pypdf
-from langchain.document_loaders import PyPDFLoader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
@@ -17,13 +15,45 @@ from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
 import re
 import pandas as pd
-import os
 
-import path
-import sys
+from langchain.docstore.document import Document
+
+if "openai_key" not in st.session_state:
+    st.session_state.openai_key = "Invalid"
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", ".", " "],
+    chunk_size=500,
+    chunk_overlap=100,
+    length_function=len,
+)
+
+llm = OpenAI(
+    model="gpt-3.5-turbo-instruct",
+    temperature=0,
+    openai_api_key=st.session_state.openai_key,
+)
+
+chain = load_qa_chain(
+    llm=llm,
+    chain_type="stuff",
+)
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-ada-002",
+    openai_api_key=st.session_state.openai_key,
+)
+
+summary_llm = ChatOpenAI(
+    model_name="gpt-3.5-turbo",
+    temperature=0.3,
+    openai_api_key=st.session_state.openai_key,
+)
 
 
 file_name = []
+
 answer = []
 
 file_match_chunk_df = pd.DataFrame({"file": [], "page no.": [], "excerpt": []})
@@ -31,96 +61,74 @@ file_match_chunk_df = pd.DataFrame({"file": [], "page no.": [], "excerpt": []})
 
 def main():
 
-    st.set_page_config(page_title="AI document intepreter")
+    file_name = []
 
-    if "openai_key" not in st.session_state:
-         st.session_state.openai_key = None
+    answer = []
 
-    st.header("Upload Your PDFs and ask me anything about them")
+    file_match_chunk_df = pd.DataFrame({"file": [], "page no.": [], "excerpt": []})
+
+    st.set_page_config(page_title="AI multi-document RAG summarise")
+
+    st.header(
+        "Enter your OpenAI API key - Upload Your PDFs - and ask me anything about them"
+    )
     user_question = st.text_input("What would you like to know from your documents?")
-    if user_question:
-        query = user_question
 
     with st.sidebar:
         st.subheader("Your OpenAI API key:")
-        KEY = st.text_input("OpenAI API Key")
+        KEY = st.text_input("please enter you OpenAI API key")
         if KEY:
-             st.session_state.openai_key = KEY
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'",
-            type="pdf",
-            accept_multiple_files=True,
-        )
-        for pdf in pdf_docs:
-            # locating local machine filepath address
-            dir = path.Path(__file__).abspath()
-            sys.path.append(dir.parent.parent)
-            # reference using  ./
-            with open("./"+uploadedfile.name, "wb") as f:
-                f.write(uploadedfile.getbuffer())
+            st.session_state.openai_key = KEY
 
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n", ".", " "],
-        chunk_size=500,
-        chunk_overlap=100,
-        length_function=len,
-    )
+            st.subheader("Your documents")
+            pdf_docs = st.file_uploader(
+                "Upload your PDFs here and proceed to input your question and click 'Process'",
+                type="pdf",
+                accept_multiple_files=True,
+            )
 
-    llm = OpenAI(
-        model="gpt-3.5-turbo-instruct",
-        temperature=0,
-        openai_api_key=st.session_state.openai_key,
-    )
+    if user_question and st.button("Process"):
+        with st.spinner("Processing"):
+            query = user_question
+            docs = []
+            file_name = []
 
-    chain = load_qa_chain(
-        llm=llm,
-        chain_type="stuff",
-    )
+            for n, pdf in enumerate(pdf_docs):
+                file_name.append(pdf.name)
+                reader = PdfReader(pdf)
 
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key=st.session_state.openai_key)
+                for i, page in enumerate(reader.pages):
+                    docs.append(
+                        Document(
+                            page_content=page.extract_text(),
+                            metadata={"page": i + 1, "source": file_name[n]},
+                        )
+                    )
+                vectorstore = FAISS.from_documents(documents=docs, embedding=embeddings)
 
-    summary_llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=0.3,
-        openai_api_key=st.session_state.openai_key,
-    )
+                matches = vectorstore.similarity_search(query, k=3)
 
-    def pdf_to_vectorstore(pdf):
-        pdf_reader = PyPDFLoader(pdf)
+                file = []
+                page = []
+                content = []
 
-        pages = pdf_reader.load_and_split(text_splitter=text_splitter)
+                for match in matches:
+                    file.append(re.sub(r".*/", "", match.metadata["source"]))
+                    page.append(match.metadata["page"])
+                    content.append(match.page_content)
 
-        vectorstore = FAISS.from_documents(documents=pages, embedding=embeddings)
-        file_name.append(re.sub(r".*/", "", pdf_reader.file_path))
-        return vectorstore
+                dict = {"file": file, "page no.": page, "excerpt": content}
+                result = pd.DataFrame(dict)
 
-    def query_vectorstore_match(query, vectorstore, k=3):
-        matches = vectorstore.similarity_search(query, k=k)
+                file_match_chunk_df = pd.concat(
+                    [file_match_chunk_df, result], ignore_index=True
+                )
 
-        file = []
-        page = []
-        content = []
-        for match in matches:
-            file.append(re.sub(r".*/", "", match.metadata["source"]))
-            page.append(match.metadata["page"] + 1)
-            content.append(match.page_content)
+                answer.append(chain.run(input_documents=matches, question=query))
 
-        dict = {"file": file, "page no.": page, "excerpt": content}
-        result = pd.DataFrame(dict)
-
-        file_match_chunk_df = pd.concat(
-            [file_match_chunk_df, result], ignore_index=True
-        )
-        return matches
-
-    def answer_query(matches, query):
-        answer.append(chain.run(input_documents=matches, question=query))
-        return
-
-    def summarise_answers(answer):
+        file_answer_df = pd.DataFrame({"file": file_name, "response": answer})
         text_to_summarize = "\n".join(answer)
-        # Make the API call to OpenAI GPT-3 for summarization
+
         chat_messages = [
             SystemMessage(
                 content=f"You are an academic research expert tasked to answer the following question: '{query}'"
@@ -130,20 +138,26 @@ def main():
             ),
         ]
         summary = summary_llm(chat_messages).content
-        return summary
+        st.write("Summarised Answer:")
+        st.write(summary)
 
-    if st.button("Process"):
-        with st.spinner("Processing"):
-            for pdf in [dir + f for f in os.listdir("./")]:
-                vectorstore = pdf_to_vectorstore(pdf)
-                matches = query_vectorstore_match(query, vectorstore, k=3)
-                answer_query(matches, query)
+        st.write("Response per file:")
+        st.dataframe(file_answer_df, hide_index=True)
+        st.download_button(
+            "Download csv",
+            file_answer_df.to_csv(),
+            file_name="file_answer.csv",
+            mime="text/csv",
+        )
 
-            file_answer_df = pd.DataFrame({"file": file_name, "answer": answer})
-            summary = summarise_answers(answer)
-            st.write(summary)
-            st.dataframe(file_answer_df, hide_index=True)
-            st.dataframe(file_match_chunk_df, hide_index=True)
+        st.write("Matching portions from files:")
+        st.dataframe(file_match_chunk_df, hide_index=True)
+        st.download_button(
+            "Download csv",
+            file_match_chunk_df.to_csv(),
+            file_name="relevant_chunks.csv",
+            mime="text/csv",
+        )
 
 
 if __name__ == "__main__":
