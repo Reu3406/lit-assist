586a39a0ac556385a825026ebba5a471af2ef1f2
Reu3406 <157335953+Reu3406@users.noreply.github.com>
2024-03-06 14:44:50 +0800

    Update LLM_multiQA_summarise_noGUI.py
    annotating script

--- LLM_multiQA_summarise_noGUI.py
+++ LLM_multiQA_summarise_noGUI.py
@@ -73,21 +73,23 @@ import os
 #input full address of pdf folder
 dir = "C:/Users/reu34/Desktop/eczema_LLMchatbot(GAIRYv4)/pdfs/"
 
-#lists all files in the folder, and append the full address path to each file name, so pdf reader can find and read files later
-pdf_docs = [dir + f for f in os.listdir(dir)]
+#lists all pdf files in the folder, and append the full address path to each file name, so pdf reader can find and read files later
+pdf_docs = [dir + f for f in os.listdir(dir) if f.endswith(".pdf")]
 
-
+#creating empty lists to fill in later 
 file_name = []
 answer = []
-
+#create empty dataframe from dictionary of column name:content list, for later fill in 
 file_match_chunk_df = pd.DataFrame({"file": [], "page no.": [], "excerpt": []})
 
+#loop through each pdf file in pdf folder
 for pdf in pdf_docs:
+    
+    pdf_reader = PyPDFLoader(pdf) #read in pdf as documents with page numbers
 
-    pdf_reader = PyPDFLoader(pdf)
+    pages = pdf_reader.load_and_split(text_splitter=text_splitter) #split text using text splitter, retains page number reference
 
-    pages = pdf_reader.load_and_split(text_splitter=text_splitter)
-
+    #alternate way to read in pdf and store as vector database, except this way no page number, every page is read and appended together into single long string of text before splitting
     # pdf_reader = PdfReader("C:/Users/reu34/Desktop/eczema_LLMchatbot(GAIRYv4)/pdfs/test.pdf")
     # file_name = pdf.metadata.title
     # file_names.append(file_name)
@@ -96,13 +98,14 @@ for pdf in pdf_docs:
     # chunks = text_splitter.split_text(text)
     # vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
 
+    #from split documents , convert to embedding vectors using embedding model and store as vector database
     vectorstore = FAISS.from_documents(documents=pages, embedding=embeddings)
 
+    #similarity search of question against chunks of text , k=3 to produce top 3 matches 
     matches = vectorstore.similarity_search(query, k=3)
 
     # alternate way to query vectorstore, but hidden similarity search step, cannot see chunk matches or control search parameters
     # from langchain.chains import RetrievalQA
-
     # qa = RetrievalQA.from_chain_type(
     #     llm=llm,
     #     chain_type="stuff",
@@ -111,34 +114,30 @@ for pdf in pdf_docs:
     # result = qa.run(query)
     # print(result)
 
+    #create empty lists for file name, page number and text content of matches to be filled in
     file = []
     page = []
     content = []
+    #loop through each matching chunk from similarity search
     for match in matches:
+        #fill in file name , page number and content , and append to respective lists
         file.append(re.sub(r".*/", "", match.metadata["source"]))
         page.append(match.metadata["page"] + 1)
         content.append(match.page_content)
-        # print(
-        #     str(match.metadata["source"]) + str(match.metadata["page"] + 1) + ":",
-        #     match.page_content[:300],
-        # )
+    #create dictionary from the lists of file name, page numnber and content, and use dictionary to append to dataframe
     dict = {"file": file, "page no.": page, "excerpt": content}
-
     file_match_chunk_df = pd.concat(
         [file_match_chunk_df, pd.DataFrame(dict)], ignore_index=True
     )
+    #append file name and summarised answer from chunks to respective lists
     file_name.append(re.sub(r".*/", "", pdf_reader.file_path))
-    answer.append(
-        chain.run(input_documents=matches, question=query)
-    )  # return on summarised answer as string
+    answer.append(chain.run(input_documents=matches, question=query))  # returns only summarised answer as string
 
-    # to return dictionary containing input documents, question and output_text
-    # answer = chain(
-    #     {"input_documents": matches, "question": query}, return_only_outputs=True
-    # )
+    # to return entire dictionary containing input documents, question and output_text
+    # answer = chain({"input_documents": matches, "question": query}, return_only_outputs=True)
 
+    #alternate way using ChromaDB instead of FAISS vectordatabase to similarity search and QA LLM
     # from langchain.vectorstores import Chroma
-
     # vectorstore = Chroma.from_documents(documents=pages, embedding=embeddings)
     # retriever = vectorstore.as_retriever(
     #     search_type="similarity", search_kwargs={"k": 6}
@@ -146,13 +145,15 @@ for pdf in pdf_docs:
     # retrieved_docs = retriever.invoke(query)
     # answer = chain.run(input_documents=retrieved_docs, question=query)
 
+#use the lists of file name and summarised answer to create dataframe 
 file_answer_df = pd.DataFrame({"file": file_name, "answer": answer})
 
 
-# llm summarise answers from each document into a single summary
-
+# summarise answers from each document into a single summarised answer
+#append all answers from each pdf into a long string
 text_to_summarize = "\n".join(answer)
-# Make the API call to OpenAI GPT-3 for summarization
+# prepare prompts to LLM to summarize all answers into final answer
+#using f strind to input question and long string of text to be summarised, into the prompt messages
 chat_messages = [
     SystemMessage(
         content=f"You are an academic research expert tasked to answer the following question: '{query}'"
@@ -161,7 +162,7 @@ chat_messages = [
         content=f"answer the question by providing a single concise summary of the following text: '{text_to_summarize}'. \n Keep the summary within 1000 words "
     ),
 ]
-
+#defining and calling LLM to summarise answers into final answer
 summary_llm = ChatOpenAI(
     model_name="gpt-3.5-turbo", temperature=0.3, openai_api_key=key
 )
@@ -169,3 +170,6 @@ summary_llm = ChatOpenAI(
 summary = summary_llm(chat_messages).content
 
 print(summary)
+
+
+
